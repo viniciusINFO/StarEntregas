9f8326f79df730c6dde7d347ce9f79a2a3bcbf99
Gueirc <41451058+Gueirc@users.noreply.github.com>
2019-06-30 17:08:27 -0300

    Implementado Dijkstra
    A função grafo_dijkstra agora existe, recebendo dois vetores que terão ao final de sua execução o custo dum ponto passado até todos os outros

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+// pra mudar pro grafo exemplar eh mudar esse define pra 4, descomentar os grafo_insere_duplo la embaixo e comentar os grafo_insere_duplo embaixo do grafo do computerphile,
+// fazer o contrario pra fazer o contrario
 
-#define QPLANETAS 4
+#define QPLANETAS 13
 
 typedef struct planeta{
     int iNdice;
@@ -15,6 +17,7 @@ plan * grafo_inicia(int iNdice);
 void grafo_insere(plan * aInserir, int iNdice, int iTempo, int bEmGuerra);
 void grafo_libera(plan * grafo);
 void grafo_insere_duplo(plan ** grafo, int prim, int sec, int iTempo, int bEmGuerra);
+void grafo_dijkstra(plan ** grafo, int gfInicio, int * iCusto, int * gfAnterior);
 
 int main(){
     srand(time(NULL));
@@ -23,45 +26,73 @@ int main(){
     for (i = 0; i < QPLANETAS; i += 1){ // Inicia cada um dos planetas
         grafo[i] = grafo_inicia(i);
     } 
-    // Criação de grafo arbitrário nao direcionado
+    // Criação de grafo arbitrário nao direcionado ( Grafo exemplar)
     // ------------------------------------------------------ //
-    //grafo_insere(grafo[0], 1, 2, 0);
-    //grafo_insere(grafo[1], 0, 2, 0);
-    grafo_insere_duplo(grafo, 0, 1, 2, 0);
+    //grafo_insere_duplo(grafo, 0, 1, 2, 0);
+    //grafo_insere_duplo(grafo, 0, 2, 5, 0);
+    //grafo_insere_duplo(grafo, 1, 2, 2, 0);
+    //grafo_insere_duplo(grafo, 2, 3, 5, 0);
+    //grafo_insere_duplo(grafo, 3, 0, 1, 0);
 
-    //grafo_insere(grafo[0], 2, 5, 0);
-    //grafo_insere(grafo[2], 0, 5, 0);
-    grafo_insere_duplo(grafo, 0, 2, 5, 0);
+    // Grafo do computerphile 
+    grafo_insere_duplo(grafo, 0, 1, 3, 0);
+    grafo_insere_duplo(grafo, 0, 3, 2, 0);
+    grafo_insere_duplo(grafo, 0, 2, 7, 0);
+    grafo_insere_duplo(grafo, 1, 4, 2, 0);
+    grafo_insere_duplo(grafo, 2, 3, 3, 0);
+    grafo_insere_duplo(grafo, 2, 7, 4, 0);
+    grafo_insere_duplo(grafo, 3, 7, 4, 0);
+    grafo_insere_duplo(grafo, 3, 9, 1, 0);
+    grafo_insere_duplo(grafo, 4, 5, 4, 0);
+    grafo_insere_duplo(grafo, 4, 6, 4, 0);
+    grafo_insere_duplo(grafo, 5, 6, 6, 0);
+    grafo_insere_duplo(grafo, 5, 10, 4, 0);
+    grafo_insere_duplo(grafo, 6, 10, 4, 0);
+    grafo_insere_duplo(grafo, 7, 8, 5, 0);
+    grafo_insere_duplo(grafo, 8, 9, 3, 0);
+    grafo_insere_duplo(grafo, 9, 11, 2, 0);
+    grafo_insere_duplo(grafo, 10, 12, 5, 0);
+    grafo_insere_duplo(grafo, 11, 12, 2, 0);
 
-    //grafo_insere(grafo[1], 2, 2, 0);
-    //grafo_insere(grafo[2], 1, 2, 0);
-    grafo_insere_duplo(grafo, 1, 2, 2, 0);
-
-    //grafo_insere(grafo[2], 3, 5, 0);
-    //grafo_insere(grafo[3], 2, 5, 0);
-    grafo_insere_duplo(grafo, 2, 3, 5, 0);
-
-    grafo_insere_duplo(grafo, 3, 0, 1, 0);
     // ------------------------------------------------------ //
-   
-    
+
     // --------------- Printa o grafo -------------------- //
     for (i = 0; i < QPLANETAS; i += 1){
         plan * aux = grafo[i];
-        printf("\n %d: ",i);
+        printf("\n %d: ",i+1);
         while (aux->prox != NULL){
-            printf("%d (%d)  ",aux->prox->iNdice,aux->prox->iTempo);
+            printf("%d (%d)  ",aux->prox->iNdice+1,aux->prox->iTempo);
             aux = aux->prox;
         }
     }
+    // ---------------------------------------------------- //
 
+    // ------------ CALCULA e printa O DIJKSTRA -------------------------// 
+    int * iCusto = malloc(sizeof*iCusto * QPLANETAS);
+    int * gfAnterior = malloc(sizeof*gfAnterior * QPLANETAS);
 
+    grafo_dijkstra(grafo, 0, iCusto, gfAnterior);
+    printf("\n Sendo o vertice inicial o de indice 0");
+    printf("\n Custo: ");
+    for (i = 0; i < QPLANETAS; i += 1){
+        printf("%d ",iCusto[i]);
+    }
+    printf("\n gfAnterior: ");
+    for (i = 0; i < QPLANETAS; i += 1){
+        printf("%d ",gfAnterior[i]);
+    }
+    printf("\n");
 
-    // ------------ Libera o grafo ----------------------------- //
+    // --------------------------------------------------- //
+
+    // ------------ Libera o grafo e as listas ----------------------------- //
     for (i = 0; i < QPLANETAS; i += 1){
         grafo_libera(grafo[i]);
     }
     free(grafo);
+    free(iCusto);
+    free(gfAnterior);
+    // ---------------------------------------------------------- //
 }
 
 plan * grafo_inicia(int iNdice){
@@ -125,8 +156,78 @@ void grafo_libera(plan * grafo){
     free(grafo);
 }
 
+void grafo_dijkstra(plan ** grafo, int gfInicio, int * iCusto, int * gfAnterior){
+
+    int * gfVisitados = malloc(sizeof*gfVisitados * QPLANETAS); // Lista de indices que ja tiveram todos vertices adjacentes visitados
+    int gfVisitadosTam = 0;
+    int * gfMenorCaminho = malloc(sizeof*gfVisitados * QPLANETAS); // Lista de indices que estao para ser testadas mas ja foram vistas por outro vertice
+    int gfMenorCaminhoTam = 0;
 
 
+    gfMenorCaminho[gfMenorCaminhoTam] = gfInicio;
+    gfMenorCaminhoTam += 1;
+    gfAnterior[gfInicio] = gfInicio;
+    iCusto[gfInicio] = 0; // Nao há distancia entre o primeiro e ele mesmo
+    int gfAtual = gfInicio;
+
+
+    while (gfMenorCaminhoTam != 0){
+        int i;
+        int minAux = 0;
+        // Escolher o que ta na lista com o menor custo >> ** em relação ao atual  ** << 
+        for (i = 1; i < gfMenorCaminhoTam; i += 1){
+            if (iCusto[gfMenorCaminho[minAux]] > iCusto[gfMenorCaminho[i]] ){ // Se o custo de tempo do atual for maior do que o de outro
+                minAux = i;                                                  // Guarda o com menor custo
+            }
+        }
+        gfAtual = gfMenorCaminho[minAux];
+        // Tira o Atual da lista
+        gfMenorCaminho[minAux] = gfMenorCaminho[gfMenorCaminhoTam -1];
+        gfMenorCaminhoTam -= 1;
+        // COloca ele na lista dos ja visitados
+        gfVisitados[gfVisitadosTam] = gfAtual;
+        gfVisitadosTam += 1;
+
+
+        plan * aux = grafo[gfAtual];
+        aux = aux->prox; // Coloca no primeiro próximo gráfico de uma vez, talvez tenha que mudar isso pra fazer contar pode esperar
+        int bJaVisitou; // Booleana se uma vertice adjacente ja foi vistada
+        int bEstaLista; // Booleana se um vertice já está na lista
+        while (aux != NULL){ // Enquanto tiver vertices adjacentes
+            bJaVisitou = 0;
+            for (i = 0; i < gfVisitadosTam; i += 1){ // Procura nos vertices ja visitados pelo atual
+                if (aux->iNdice == gfVisitados[i]){
+                    bJaVisitou = 1; // Se achar só passa pro próximo vertice
+                }
+            }
+            if (bJaVisitou != 1){
+                bEstaLista = 0;
+                for (i = 0; i < gfMenorCaminhoTam; i += 1){ // Se for um dos grafos que esta na lista
+                    if (aux->iNdice == gfMenorCaminho[i]){ // Se sim existe outro caminho e testamos qual o menor
+                        bEstaLista = 1;
+                        if (iCusto[gfAtual] + aux->iTempo < iCusto[gfMenorCaminho[i]]){ // Se sim o que acabamos de descobrir eh menor
+                            iCusto[gfMenorCaminho[i]] = iCusto[gfAtual] + aux->iTempo; // O novo menor eh esse
+                            gfAnterior[gfMenorCaminho[i]] = gfAtual; // O anterior ao desse outro caminho eh o atual
+                        }
+                    break;
+                    }
+
+                }
+                if (bEstaLista != 1){ // Se nao estiver na lista a gente coloca na lista(menorcaminho) e nas outras coisas
+                    gfMenorCaminho[gfMenorCaminhoTam] = aux->iNdice;
+                    gfMenorCaminhoTam += 1;
+                    iCusto[aux->iNdice] = aux->iTempo + iCusto[gfAtual]; // O custo do atual eh o custo do atual (o mais curto ate agr) e o caminho
+                    gfAnterior[aux->iNdice] = gfAtual;
+                }
+
+            }
+
+            aux = aux->prox;
+        }
+
+
+    }
+}
 
 
 
